10c2e239adbfa2eef3434f9a3a3a678bd4019a39
Alex Sexton <alex@pierre.co>
2025-09-30 17:25:46 -0500

    real shad registry (#15)
    * get rid of older examples, create one tiny component
    
    * clean up cruft, make registry work

---
+++ apps/docs/app/shadcn/page.tsx
@@ -0,0 +1,31 @@
+import { OpenInV0Button } from '@/components/open-in-v0-button';
+import { GitPlatformSync } from '@/registry/blocks/git-platform-sync/git-platform-sync';
+
+export default function Home() {
+  return (
+    <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
+      <header className="flex flex-col gap-1">
+        <h1 className="text-3xl font-bold tracking-tight">
+          Pierre Shadcn Component Registry
+        </h1>
+        <p className="text-muted-foreground">
+          This custom registry mimics the shadcn registry to allow for the
+          distribution of our components using the shadcn tooling.
+        </p>
+      </header>
+      <main className="flex flex-col flex-1 gap-8">
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              Git Platform Sync
+            </h2>
+            <OpenInV0Button name="hello-world" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <GitPlatformSync />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
